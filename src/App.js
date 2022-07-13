738d47e6747b4f7445743b876d777010ea5d4327
BiteSnail <rkwoal216@gmail.com>
2022-07-13 09:10:57 +0900

    add : props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent></MyComponent>;
+  return <MyComponent name="react" favoriteNumber={3}><h1>리액트</h1></MyComponent>;
 };
 
 export default App;
