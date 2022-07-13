739df5ca703bd01dcf75ff52f856ac12b3973a02
BiteSnail <rkwoal216@gmail.com>
2022-07-13 10:48:27 +0900

    use state example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import MyComponent from "./MyComponent";
 import Counter from "./Counter";
-
+import Say from "./Say"
 const App = () => {
   return (
   <div>
     <MyComponent name="react" favoriteNumber={3}><h1>리액트</h1></MyComponent>
     <Counter></Counter>
+    <Say></Say>
   </div>
   );
 
