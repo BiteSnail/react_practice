e055187c5d3792b8ee45b89da300e8ff6ccf607a
BiteSnail <rkwoal216@gmail.com>
2022-07-24 18:50:42 +0900

    useState Sample

--- src/Counter.js
+++ src/Counter.js
@@ -1,42 +1,17 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 
-class Counter extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            number: 0,
-            fixedNumber : 0
-        };
-    };
-    render(){
-        const { number, fixedNumber } = this.state;
-        return (
-            <div>
-                <h1>{number}</h1>
-                <h2>바뀌지 않는 값: {fixedNumber}</h2>
-                <button
-                    onClick={() => {
-                        this.setState(
-                            {
-                                number : number +1
-                            },
-                            ()=>{
-                                console.log('call setState.');
-                                console.log(this.state);
-                            }
-                        )
-                    }}
-                > +1 </button>
-            </div>
-        )
-    }
-    add(){
-        this.setState( prevState => { 
-            return {
-                number: prevState.number + 1
-            };
-        });
-    }
+const Counter = () => {
+    const [value, setValue] = useState(0);
+
+    return (
+        <div>
+            <p>
+                현재 카운터 값은 <b>{value}</b>입니다.
+            </p>
+            <button onClick={() => setValue(value + 1)}>+1</button>
+            <button onClick={() => setValue(value - 1)}>-1</button>
+        </div>
+    )
 }
 
 export default Counter;
