a868aed20a8b8e42783ce7c7ac138d072ada4d39
bitesnail <rkwoal216@gmail.com>
2022-07-30 14:45:06 +0900

    yarn add classnames

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,7 +1,7 @@
 import styles from './CSSModule.module.css';
 const CSSModule = () => {
     return (
-        <div className={styles.wrapper}>
+        <div className={`${styles.wrapper}  ${styles.inverted}`}>
             Hi, i'm <span className="something">CSS Module!</span>
         </div>
     );
